520ebffc2cfd498af670b470868abf0775e29376
Amaechi McMillan <amaechimcmillan@gmail.com>
2022-10-12 18:11:39 -0500

    added post

--- server.js
+++ server.js
@@ -1,44 +1,38 @@
-const express = require("express")
+const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
-const db = require("./db/db.json")
+const db = require("./db/db.json");
 const app = express();
+const fs = require("fs");
 
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(express.static("public"));
 
-app.use(express.static('public'));
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
 
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/apiNotes.js"))
+);
 
-app.get("/notes", (req, res) =>{
+app.get("/api/notes", (req, res) => {
+  res.json(db);
+});
 
+app.post("/api/notes", (req, res) => {
+  db.push(req.body);
+  fs.writeFileSync("./db/db.json", JSON.stringify(db));
+  res.json(db);
+});
 
-    res.sendFile(path.join(__dirname, "/public/notes.html"));
+// app.delete("/api/notes", (req, res) =>{
+//   res.json(db);
+// })
 
-
-})
-
-app.get("/api/notes", (req, res) =>{
-
-
-
-
-
-
-})
-
-app.listen(PORT, ()=> console.log("listening on port http://localhost:"+PORT))
-
-
-
-
-
-
-
-
-
-
-
-
+app.listen(PORT, () =>
+  console.log("listening on port http://localhost:" + PORT)
+);
